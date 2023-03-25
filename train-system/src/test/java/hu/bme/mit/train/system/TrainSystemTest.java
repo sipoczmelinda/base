2fe5bb87d629a2129b326ef69762fd586d713879
Sipőcz Melinda Emese <sipoczmelinda@gmail.com>
2023-03-25 16:47:43 +0100

    Error corrected + unit test for emergency brake

--- train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
+++ train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
@@ -50,5 +50,9 @@ public class TrainSystemTest {
 		Assert.assertEquals(0, controller.getReferenceSpeed());
 	}
 
-	
+	@Test
+	public void EmergencyBraking(){
+		user.emergencyBraking();
+		Assert.assertEquals(0, sensor.getReferenceSpeed());
+	}
 }
